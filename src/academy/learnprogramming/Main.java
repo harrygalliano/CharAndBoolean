1b5a78ab1c3c5f71e0400c7b47555e73afae0da5
Harry Galliano <harrygalliano97@gmail.com>
2019-09-27 18:18:17 +0100

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char uniCodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(uniCodeChar);
+        char copyRight = '\u00A9';
+        System.out.println(copyRight);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
